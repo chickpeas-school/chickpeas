6980c10b83f955c9824e6d998e404933760c6594
arnerjohn <brad@uniteus.com>
2019-01-06 00:07:18 -0500

    Adding a Select All

---
+++ app/assets/javascripts/mass_messages.js
@@ -0,0 +1,14 @@
+$(function() {
+  $("#mass-message-select-all").on("click", function() {
+    var elem = $(this);
+
+    parentCheckboxes = $(".mass-message-parent-select");
+    if (parentCheckboxes.prop("checked")) {
+      elem.html("Select All");
+      parentCheckboxes.prop("checked", false);
+    } else {
+      elem.html("Deselect All");
+      parentCheckboxes.prop("checked", true);
+    }
+  });
+});
