a05e2fef73c7cebaf118ca2178174f9747eaffc4
arnerjohn <brad@uniteus.com>
2019-01-14 00:19:38 -0500

    Adding order statement

--- app/models/saleable_day.rb
+++ app/models/saleable_day.rb
@@ -4,7 +4,7 @@ class SaleableDay < ApplicationRecord
 
   scope :for_sale, -> { where(buyer: nil) }
   scope :sold, -> { where.not(buyer: nil) }
-  scope :upcoming, -> { where("date > ?", Time.now) }
+  scope :upcoming, -> { where("date > ?", Time.now).order(date: :asc) }
 
   self.per_page = 10
 
