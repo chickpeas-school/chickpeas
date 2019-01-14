20ffb372414610e0e5056c84ffa1fb6b88a4d396
arnerjohn <brad@uniteus.com>
2019-01-13 22:46:38 -0500

    Adding comments to Email Config

--- app/models/email_config.rb
+++ app/models/email_config.rb
@@ -1,11 +1,41 @@
+# EmailConfig was built with the intention of testing the SaleableDayMailer in `production` without
+# email everyone within the system. There are 2 primary pieces of information that are relevant:
+# 1. Is it for `testing`?
+# 2. What is the main distribution email address for annoucing that a day has been put up for sale? At present, parents@chickpeas.org is an email group address that distributes emails to all the parents.
+#
+# Example for testing:
+# ```
+# {
+#   email: "brad@prudl.com",
+#   testing: true,
+#   genre: "saleable_days",
+#   description: "default testing email config"
+# }
+# ```
+#
+# Example for live:
+# ```
+# {
+#   email: "parents@chickpeas.com",
+#   testing: false,
+#   genre: "saleable_days",
+#   description: "default production email config"
+# }
+# ```
+#
+SALEABLE_DAYS_GENRE = "saleable_days"
+
 class EmailConfig < ApplicationRecord
-  scope :saleable_day, -> { where(genre: "saleable_days").limit(1).first }
+
+  scope :saleable_days_config, -> { where(genre: SALEABLE_DAYS_GENRE).limit(1).first }
 
   def self.saleable_days_in_test_mode?
-    self.saleable_day && self.saleable_day.testing
+    self.saleable_days_config && self.saleable_days_config.testing
   end
 
+  # saleable_distribution_email checks to see if the EmailConfig.saleable_days_config in the DB
+  # has an email address. If it does have an email address, then that is the email address returned
   def self.saleable_distribution_email
-    self.saleable_day && self.saleable_day.email
+    self.saleable_days_config && self.saleable_days_config.email
   end
 end
