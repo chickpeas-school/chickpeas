0088fa58ecf697b79c476d9ce22fe484ca1383a6
arnerjohn <brad@uniteus.com>
2019-06-19 00:16:14 -0400

    Buy first day posted

--- app/models/saleable_day.rb
+++ app/models/saleable_day.rb
@@ -10,6 +10,17 @@ class SaleableDay < ApplicationRecord
 
   validates :date, presence: true
 
+  class << self
+    def find_first_on_date(saleable_day_id)
+      days = SaleableDay.all
+      saleable_day = days.find(saleable_day_id)
+      saleable_day_date = saleable_day.date.to_date
+
+      other_days = days.reject { |d| d.id.eql?(saleable_day_id) }
+      other_days.select { |d| saleable_day_date == d.date.to_date }.sort_by(&:created_at).first || saleable_day
+    end
+  end
+
   def start_time
     date
   end
