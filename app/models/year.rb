35095d2b2a8002ff286119cdfcb007b0a148deea
arnerjohn <brad@uniteus.com>
2019-07-15 00:37:06 -0400

    Add before_create to ensure unique current_year

--- app/models/year.rb
+++ app/models/year.rb
@@ -1,11 +1,24 @@
 class Year < ApplicationRecord
   has_and_belongs_to_many :children
 
-  def self.current_year
-    find_by(current_year: true)
+  before_create :ensure_unique_current_year
+
+  class << self
+    def current_year
+      find_by(current_year: true)
+    end
+
+    def current_parents
+      current_year.children.map(&:parents).flatten.uniq
+    end
   end
 
-  def self.current_parents
-    self.current_year.children.map(&:parents).flatten.uniq
+  def ensure_unique_current_year
+    if current_year
+      Year.where(current_year: true).each do |y|
+        y.current_year = false
+        y.save
+      end
+    end
   end
 end
