0b0bcc88b1cd52bfaf6b101d9a7cb827a8b10677
arnerjohn <brad@uniteus.com>
2019-07-24 22:54:44 -0400

    Add parent to child

--- app/controllers/children/parents_controller.rb
+++ app/controllers/children/parents_controller.rb
@@ -2,20 +2,38 @@ class Children::ParentsController < ApplicationController
   before_action :set_child
 
   def new
-
+    @parent = @child.parents.new
   end
 
   def create
+    @parent = @child.parents.create(parent_params)
 
+    respond_to do |format|
+      if @parent.save
+        format.html { redirect_to child_path(@child), notice: "A Parent was successfully added to: #{@child.name}"}
+        format.json { render :show, status: :created, location: @child }
+      else
+        format.html { render :new }
+        format.json { render json: @child.errors, status: :unprocessable_entity}
+      end
+    end
   end
 
   def destroy
-
+    @parent = @child.parents.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to child_path(@child), notice: "The parent was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
 
   def set_child
     @child = Child.find(params[:child_id])
+  end
+
+  def parent_params
+    params.permit(:name, :email, :phone_number, :job, :address)
   end
 end
