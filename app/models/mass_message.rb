8d88003cc05e9166ae23aca22a673d53184f4df4
arnerjohn <brad@uniteus.com>
2019-07-26 00:02:58 -0400

    Adding comment for Mass Messages

--- app/models/mass_message.rb
+++ app/models/mass_message.rb
@@ -2,6 +2,13 @@ class MassMessage < ApplicationRecord
   has_and_belongs_to_many :parents
 
   def send_sms
+    #
+    # The AWS Client uses Environmental Variables:
+    # AWS_ACCESS_KEY_ID
+    # AWS_SECRET_ACCESS_KEY
+    #
+    # The app uses a `.env` file in order to set these Environmental Variables
+    #
     aws_client = Aws::SNS::Client.new
     parents.each do |parent|
       resp = aws_client.publish(
